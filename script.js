7ed23fcb3e992e673fde58187cf6b7aeb1f713e5
yujeiry <43036901+yujeiry@users.noreply.github.com>
2019-03-28 18:50:09 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+ $("#profile-picture").click(function(){
+     alert("hello");
+    $("#sidebar").css("background-color", "aqua");
+  });
+  
+  $(".post-image").click(function(){
+    $(".post-text").css("background-color", "magenta");
+    alert("bye");
+  });
+  
+  $("#wall").click(function(){
+    $("#wall").hide();
+    alert("what?");
+});
+
+$("#header").click(function(){
+    $("#header").css("background-color", "pink");
+  });
+
